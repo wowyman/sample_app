f28aceaf55981545be0a9a8c2d4d4919bc8ed324
Danh <maicongdanh29012001@gmail.com>
2022-05-31 13:19:07 +0700

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
 module ActiveSupport
   class TestCase
@@ -11,6 +11,10 @@ module ActiveSupport
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
 
     # Add more helper methods to be used by all tests here...
   end

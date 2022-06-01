330cc332bc7bd6e8d9f525bc84fdd2382de74a41
Danh <maicongdanh29012001@gmail.com>
2022-06-01 15:53:31 +0700

    Finish user edit, update,index, destroy action

--- Gemfile
+++ Gemfile
@@ -24,7 +24,10 @@ gem "jbuilder", "~> 2.7"
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'erb-formatter'
+gem 'faker', '2.11.0'
+gem 'will_paginate', '3.3.0'
+gem 'bootstrap-will_paginate', '~> 1.0' 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'rails-controller-testing'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #log user
+      forwarding_url = session[:forwarding_url]
       reset_session
       log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      redirect_to forwarding_url || user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render "new"

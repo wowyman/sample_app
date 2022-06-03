18814021f88805dd63ee4811643c38be0fbe7696
Danh <maicongdanh29012001@gmail.com>
2022-06-03 09:44:43 +0700

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "evening-savannah-85814.herokuapp.com"
+  host = "https://evening-savannah-85814.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => "smtp.sendgrid.net",
-    :port => "587",
+    :port => 587,
     :authentication => :plain,
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],

e3bdb256652d9638924b3a3784feee957196d687
Danh <maicongdanh29012001@gmail.com>
2022-06-08 16:24:53 +0700

    Add following user

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,6 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end

3fb3c2cb4282e64198ff53e1ab49e64f7c16cd2b
Danh <maicongdanh29012001@gmail.com>
2022-06-20 08:21:07 +0700

    dem project tro ve truoc khi lam

--- db/migrate/20220617092458_add_fields_to_user.rb
+++ db/migrate/20220617092458_add_fields_to_user.rb
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 # frozen_string_literal: true
 
+=======
+>>>>>>> eeca827... Add google and facebook authentication
 class AddFieldsToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :google_token, :string

60fef5317bdfe2a90760c8e7b9ff09a85801f9a2
Danh <maicongdanh29012001@gmail.com>
2022-08-05 15:33:59 +0700

    add user-interactive test

--- test/models/user_interactive_test.rb
+++ test/models/user_interactive_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserInteractiveTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @user_interactive = @user.user_interactive.build()
+  end
+
+  test "should be valid" do
+    assert @user_interactive.valid?
+  end
+
+  test "user id should be present" do
+    @user_interactive.user_id = nil
+    assert_not @user_interactive.valid?
+  end
 end
